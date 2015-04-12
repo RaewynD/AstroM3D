8ebc53635c6c099ebf31d46f03456ea8c93c0282
RaewynD <RaewynD@gmail.com>
2015-04-12 10:01:07 -0400

    set up server test

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(request,response) {
+        response.set('Content-Type', 'text/html');
+        response.send(index.html);
+});
+
+app.listen(process.env.PORT || 3000);
